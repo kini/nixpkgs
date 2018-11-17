008815a803ce45cef7c27c52ae7f7b6312788df1
Florian Klink <flokli@flokli.de>
2018-11-17 17:44:10 +0100

    virtualboxExtpack: update to be a drv with required directory structure

--- pkgs/applications/virtualization/virtualbox/extpack.nix
+++ pkgs/applications/virtualization/virtualbox/extpack.nix
@@ -1,21 +1,26 @@
-{ fetchurl, lib, virtualbox }:
+{ fetchurl, stdenv, virtualbox }:
 
-with lib;
-
-let
+stdenv.mkDerivation rec {
+  pname = "Oracle_VM_VirtualBox_Extension_Pack";
   inherit (virtualbox) version;
-in
-fetchurl rec {
-  name = "Oracle_VM_VirtualBox_Extension_Pack-${version}.vbox-extpack";
-  url = "https://download.virtualbox.org/virtualbox/${version}/${name}";
-  sha256 =
-    # Manually sha256sum the extensionPack file, must be hex!
-    # Thus do not use `nix-prefetch-url` but instead plain old `sha256sum`.
-    # Checksums can also be found at https://www.virtualbox.org/download/hashes/${version}/SHA256SUMS
-    let value = "cab1abad478679fc34a0c5cb4a6d3566edc20e3c54cbed39c8e895d8cfad3ee2";
-    in assert (builtins.stringLength value) == 64; value;
 
-  meta = {
+  src = fetchurl {
+    url = "http://download.virtualbox.org/virtualbox/${version}/${pname}-${version}.vbox-extpack";
+    sha256 = "0i74s6lcn8alksk3vvg4cbqdk5nlic3r55npyk60ljv581lrb817";
+  };
+
+  unpackCmd = ''
+    mkdir -p out
+    tar xf $curSrc -C out
+  '';
+  dontBuild = true;
+
+  installPhase = ''
+    mkdir -p $out/${pname}
+    cp -R * $out/${pname}
+  '';
+
+  meta = with stdenv.lib; {
     description = "Oracle Extension pack for VirtualBox";
     license = licenses.virtualbox-puel;
     homepage = "https://www.virtualbox.org/";
